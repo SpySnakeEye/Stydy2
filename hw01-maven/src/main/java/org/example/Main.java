1a2c49c01f54b68905a4ac4d57c64e53815c722c
SpySnakeEye <art.arh1@gmail.com>
2022-10-30 19:06:10 +0300

    Second commit

--- hw01-maven/src/main/java/org/example/Main.java
+++ hw01-maven/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
-public class Main {
+
+import com.google.common.base.Functions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Lists;
+import java.util.List;
+
+public class FluentIterableEx {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Car> cars = Lists.newArrayList(new Car(1, "Audi", 52642),
+                new Car(2, "Mercedes", 57127), new Car(3, "Skoda", 9000),
+                new Car(4, "Volvo", 29000));
+
+        Predicate<Car> byPrice = car -> car.price() <= 30000;
+
+        List<String> results = FluentIterable.from(cars)
+                .filter(byPrice)
+                .transform(Functions.toStringFunction())
+                .toList();
+
+        System.out.println(results);
     }
 }
+
+record Car(int id, String name, int price) {}
